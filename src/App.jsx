0c376326191796d4392fee8f36d975d738b410ab
Ana de Almeida <ana.isabel190@gmail.com>
2020-05-25 14:06:19 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 
 import './App.css';
 
@@ -7,20 +7,44 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <nav className="navbar">
+            <img src="images/ironhack-logo.svg" alt="ironhack logo" />
+            <img src="images/menu-top.svg" alt="menu logo" />
+         </nav>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          
+          <h1>Say hello to ReactJS</h1>
+          <p>You will learn a Frontend framework from scratch, to become an Ninja Developer.</p>
+          <br />
           <a
             className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Awesome!
           </a>
         </header>
+        <section className="section-2">
+          <div className="box">
+            <img src="images/icon1.png" alt="declarative icon" />
+            <h4>Declarative</h4>
+            <p>React makes it painless to create interactive UIs</p>
+          </div>
+          <div className="box">
+            <img src="images/icon2.png" alt="components icon" />
+            <h4>Components</h4>
+            <p>Build encapsulated components that manage their state</p>
+          </div>
+          <div className="box">
+            <img src="images/icon3.png" alt="single-way icon" />
+            <h4>Single-Way</h4>
+            <p>A set of immutable values are passed to the component's</p>
+          </div>
+          <div className="box">
+            <img src="images/icon4.png" alt="jsx icon" />
+            <h4>JSX</h4>
+            <p>Statically-typed, designed to run on modern browsers.</p>
+          </div>
+        </section>
       </div>
     );
   }
